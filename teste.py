88c602b5aa8014ffbb1fb0745f0f63c69ea336ba
Matheus Ferreira de Souza <151486309+matheusf30@users.noreply.github.com>
2023-12-05 13:19:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,8 +4,11 @@ drive.mount('/content/drive')
 os.chdir("/content/drive/MyDrive/")
 !ls
 
-import pandas as pd
+import pandas as pd # clássicas
+import numpy as np  # clássicas
+import matplotlib.pyplot as plt  # clássicas
 import datetime
+from random import randrange, seed
 
 # criando um objeto datetime com a data e hora atual
 agora = datetime.datetime.now()
@@ -21,3 +24,26 @@ data_2 = datetime.date(2023, 1, 1)
 # calculando a diferença entre as duas datas
 diferenca = data_2 - data_1
 print("Diferença entre as duas datas:", diferenca)
+
+# listando tipos diferentes de variáveis
+lista = ['int', False, True, '18', 2020]
+for elemento in lista:
+    print(type(elemento))
+
+# criando lista de nostas aleatórias
+seed(10)
+notas = []
+for nota in range(0, 18):
+    notas.append([randrange(0,11)])
+print(notas)
+print(len(notas))
+print(type(randrange))
+
+# visualização gráfica (dados aleatórios)
+x = list(range(1,100))
+y = list(range(1,100))
+plt.plot(x, y, marker='o')
+plt.title('Notas de matemática')
+plt.xlabel('Provas')
+plt.ylabel('Notas')
+plt.show()
